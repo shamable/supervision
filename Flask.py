2a3a1058edb6b0143e26beb9d6ba4929373c340e
Balgor18 <catinaud.florian@gmail.com>
2020-03-06 22:29:55 +0100

    Petite retouche

--- Flask.py
+++ Flask.py
@@ -5,49 +5,15 @@ from datetime import datetime, timedelta
 import socket
 import os
 from pprint import pprint
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-# import smtplib,email,email.encoders,email.mime.text,email.mime.base
-from Database import insertValue,selectValue,deleteValue , deleteTable , createTable , selectSpecificValue
-from templates_email import debut_email , end_email
+from Database import insertValue , selectValue,deleteValue , deleteTable , createTable , selectSpecificValue
+from envoie_mail import sendEmail
+# testsupervision2020@gmail.com
+# testsupervision2!
+
+# supervision.rasberry@gmail.com
+# rasberrysupervision2020
 
 app= Flask(__name__)
-
-def sendEmail (text,type,etat):
-	jour = datetime.now()
-	heure = jour.hour
-	minute = jour.minute
-	value = selectValue(False)
-	msg = MIMEMultipart()
-	msg['From'] = 'supervision.rasberry@gmail.com'
-	msg['To'] = 'i.loubani89@gmail.com'
-	msg['Subject'] = 'Probleme Supervision Serveur '+type+' '+etat+' '+str(heure)+':'+str(minute)+'\n'
-	message = debut_email()
-	message = 'Bonjour !'+ '<br />'
-	message += text
-	message += '<br />'
-	message += '<b>PROBLEME :</b>'+'<br />'
-	if type== 'temperature':
-		message += 'Le serveur possede un probleme sur la '+type+' '+str(getTemp())+'°C'
-	elif type == 'Humidité':
-		message += 'Le serveur possede un probleme sur l\''+type+' '+str(getHumidity())+' %'
-	elif type == 'Pression':
-		message += 'Le serveur possede un probleme sur la '+type+' '+str(getPressure())+' mBar'
-	message += '<br />'
-	message += 'Merci de reglé le probleme'
-	message += '<br />'
-	message += 'Florian Catinaud'
-	message += end_email()
-	msg.attach(MIMEText(message,'html'))
-	mailserver = smtplib.SMTP('smtp.gmail.com', 587)
-	mailserver.ehlo()
-	mailserver.starttls()
-	mailserver.ehlo()
-	mailserver.login('supervision.rasberry@gmail.com', 'rasberrysupervision2020')
-	mailserver.sendmail(msg['From'], msg['To'], msg.as_string())
-	mailserver.quit()
-	print('Envoie de mail à '+msg['To']+' '+str(type)+' '+str(etat))
-	return
 
 @app.route("/gT/<rep>/<date>")
 def graTemp (rep,date):
@@ -119,4 +85,4 @@ def home():
 		)
 
 if __name__ =="__main__":
-	app.run(debug=True,host=adresseIP(),port='5000')
+	app.run(debug=False,host=adresseIP(),port='5000')
